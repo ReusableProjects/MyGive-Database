b6b9d28ad56af8b2fe77841afdad48916474a8ca
tlfzgjadyd <tlfzgjadyd@naver.com>
2021-09-09 00:40:59 +0900

    sql

---
+++ Sql/KBbankDB/CardHistoryDummy.sql
@@ -0,0 +1,23 @@
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('1111111111111111', 0, '적십자', '2020-09-04 15:00:01', 100000);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('1111111111111111', 1, '관악구청', '2020-11-01 11:43:34', 5000);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('1111111111111111', 2, '바른 유치원', '2020-12-17 10:12:35', 20000);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('1111111111111111', 3, '유령회사', '2021-01-02 15:29:00', 1000);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('1111111111111111', 4, '동작구 보건소', '2021-02-28 08:01:10', 15000);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('1111111111111111', 5, '비둘기 협회', '2021-05-20 15:09:51', 5000);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('1111111111111111', 6, '숭실대학교', '2021-07-10 17:11:41', 10000);
+
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('2222222222222222', 0, '적십자', '2019-01-04 08:12:51', 100000);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('2222222222222222', 1, '관악구청', '2019-12-01 20:43:43', 50000);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('2222222222222222', 2, '바른 유치원', '2020-03-17 11:12:50', 12000);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('2222222222222222', 3, '유령회사', '2020-07-09 15:31:02', 500);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('2222222222222222', 4, '동작구 보건소', '2021-01-20 08:30:42', 1000);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('2222222222222222', 5, '비둘기 협회', '2021-03-17 17:49:14', 10000);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('2222222222222222', 6, '숭실대학교', '2021-08-10 10:11:01', 100000);
+
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('3333333333333333', 0, '적십자', '2018-12-30 09:02:34', 100000);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('3333333333333333', 1, '관악구청', '2019-03-20 12:34:20', 50000);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('3333333333333333', 2, '바른 유치원', '2019-08-14 06:15:20', 12000);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('3333333333333333', 3, '유령회사', '2019-10-01 10:25:00', 500);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('3333333333333333', 4, '동작구 보건소', '2020-07-21 08:20:01', 1000);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('3333333333333333', 5, '비둘기 협회', '2020-12-04 10:43:58', 10000);
+INSERT INTO CardHistory(cardNum, historyNum, organizationName, dateTime, price) VALUES('3333333333333333', 6, '숭실대학교', '2021-04-29 11:21:02', 100000);
--- Sql/KBbankDB/KBbankDBSqlCode.sql
+++ Sql/KBbankDB/KBbankDBSqlCode.sql
@@ -1,3 +1,5 @@
+#국민은행 카드사의 api는 사업자 등록 후에 이용할 수 있기에
+#차후 api를 교체할때를 대비하여 만들어둔 코드입니다
 create database KBbank;
 use KBbank
 create user 'myGive'@'%' identified by 'myGive';
